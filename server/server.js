de6f9a5d0f299e4c62c6fab6003cab72907a622c
kimiko <suryanshi180504@gmail.com>
2026-05-06 04:12:20 +0530

    fixed server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 require("dotenv").config();
-
 
 const app = express();
 
@@ -11,25 +11,23 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("MongoDB Connected"))
-.catch(err=>console.log(err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
+// API routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/tasks", require("./routes/tasks"));
 app.use("/api/projects", require("./routes/projects"));
 app.use("/api/users", require("./routes/users"));
 
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
-const path = require("path");
-
-// Serve frontend
-app.use(express.static(path.join(__dirname, "../client/build")));
-
-// React routing fix
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
